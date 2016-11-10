52a4ac4aa31a3015c937b2c06413d56f97fd40ff
Oriel Zambrano <oristides@gmail.com>
2016-11-10 20:01:11 -0200

    Update and rename pending.R to function_graphvector.R

---
+++ R/function_graphvector.R
@@ -0,0 +1,12 @@
+url<-"https://github.com/oristides/vertcloseness/blob/master/edges.dat"
+
+edges<-read.table(url)
+social <- graph.data.frame(edges)
+
+closeness_by_name<-function(vertice_name) { if(class(vertice_name)=="numeric"){
+  verticechar<-as.character(vertice_name)
+  answer<-closeness(social)[verticechar]} else{ 
+    numvect<-as.character(( as.numeric(read.csv(vertice_name, sep = " ", header = F))))
+    answer<-closeness(social)[numvect]    
+  }
+  return(answer)}
